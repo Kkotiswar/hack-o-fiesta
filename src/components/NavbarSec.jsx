57a7f15e2e0ca0e2ef9a02d10133222c494f547b
Ankit kumar <lit2020034@iiitl.ac.in>
2022-04-17 22:26:57 +0530

    dsfd

---
+++ src/components/NavbarSec.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+ // import react-bootstrap css file
+    import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Navbar, Nav, NavDropdown, Button } from 'react-bootstrap';
+
+const NavbarSec = () => {
+    return (
+      
+//       <Navbar collapseOnSelect expand="lg" style={{backgroundColor:'white',color:'black'}}>
+//   <Container>
+
+//   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+//   <Navbar.Collapse id="responsive-navbar-nav">
+//     <Nav className="me-auto" style={{marginLeft:'25%'}}>
+//       <Nav.Link href="#features">Home</Nav.Link>
+//       <NavDropdown title="Practice" id="collasible-nav-dropdown">
+//         <NavDropdown.Item href="#action/3.1">Problems</NavDropdown.Item>
+//         <NavDropdown.Divider />
+//         <NavDropdown.Item href="#action/3.2">Contests</NavDropdown.Item>
+//       </NavDropdown>
+//       <Nav.Link href="#pricing">Discuss</Nav.Link>
+//       <Nav.Link href="#pricing">Visualiser</Nav.Link>
+//       <NavDropdown title="Learn" id="collasible-nav-dropdown">
+//         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+//         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+//         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+//         <NavDropdown.Divider />
+//         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+//       </NavDropdown>
+//     </Nav>
+//   </Navbar.Collapse>
+//   </Container>
+// </Navbar>
+{/* <div>
+
+<Navbar bg="dark" variant="dark" >
+  <Container>
+    <Navbar.Brand href="#home">vCode</Navbar.Brand>
+    <Button  variant="outline-secondary">New User / Login</Button>
+  </Container>
+</Navbar>
+
+<Navbar className='navbar' bg="secondary" variant="dark">
+  <Container>
+    <Navbar.Brand href="#home"></Navbar.Brand>
+    <Nav className="me-auto nav_class">
+      <Nav.Link className='nav_item_class' href="#home">Home</Nav.Link>
+      <NavDropdown title="Practice" id="navbarScrollingDropdown">
+        <NavDropdown.Item className='nav_item_class' href="#action3">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action5">
+          Something else here
+        </NavDropdown.Item>
+      </NavDropdown>
+      <Nav.Link className='nav_item_class' href="#discuss">Discuss</Nav.Link>
+      <Nav.Link className='nav_item_class' href="#visualizer">Visualizer</Nav.Link>
+      <NavDropdown title="Learn" id="navbarScrollingDropdown">
+        <NavDropdown.Item className='nav_item_class' href="#action3">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action5">
+          Something else here
+        </NavDropdown.Item>
+      </NavDropdown>
+      <NavDropdown className='nav_item_class' title="More" id="navbarScrollingDropdown">
+        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action5">
+          Something else here
+        </NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+  </Container>
+</Navbar>
+
+</div> */}
+    );
+};
+
+export default NavbarSec;
---
+++ src/components/Temp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Temp = (props) => {
+  return (
+    <div>{props.text}</div>
+  )
+}
+
+export default Temp;
